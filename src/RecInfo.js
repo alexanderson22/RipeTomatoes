c2f6f237bf00cd5fa63cd3340a29750aa80f3200
Z Anderson <aander79@lion.lmu.edu>
2022-12-01 14:13:22 -0800

    working on the recInfo and movie Info

--- src/RecInfo.js
+++ src/RecInfo.js
@@ -1,18 +1,22 @@
-export default function recInfo({
-    recData
+export default function RecInfo({
+    recData, movieId
   }) {
     return !recData ? (
       <p></p>
-    ) : !recData.Title ? (
-      <p>{recData.Title} cannot be found</p>
+    ) : !recData.page.results[ranNum].title ? (
+      <p></p>
     ) : (
       <div>
         <ul>
-          <li>{recData.Title}</li>
-          <li>
-            
-          </li>
+          {recData.page.results[ranNum].map(() => (
+            <li> key={} </li>
+          ))}
         </ul>
       </div>
     );
   }
+
+  // make a random number generator between 0 and 39 for index of results.
+
+  // for loop needed to get the 5 or 10 recommendations
+
